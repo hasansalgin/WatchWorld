d75dc0b9e8d1ecc0d1824b17ceed8b0d207b3492
hasan salgin <hasansalgin66@gmail.com>
2024-03-04 12:29:32 +0300

    home index , viewmodels

---
+++ src/Web/Interfaces/IHomeViewModelService.cs
@@ -0,0 +1,9 @@
+﻿using Web.Models;
+
+namespace Web.Interfaces
+{
+    public interface IHomeViewModelService
+    {
+       public Task<HomeViewModel> GetHomeViewModelAsync(int? categoryId, int? brandId);
+    }
+}
