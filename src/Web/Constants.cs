a2cf5190434e2d5c6cb9570af5009597659dc319
hasan salgin <hasansalgin66@gmail.com>
2024-03-05 09:38:01 +0300

    Pagination spec

---
+++ src/Web/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Web
+{
+    public class Constants
+    {
+        public const int ITEMS_PER_PAGE = 4;
+    }
+}
--- src/Web/Interfaces/IHomeViewModelService.cs
+++ src/Web/Interfaces/IHomeViewModelService.cs
@@ -4,6 +4,6 @@ namespace Web.Interfaces
 {
     public interface IHomeViewModelService
     {
-       public Task<HomeViewModel> GetHomeViewModelAsync(int? categoryId, int? brandId);
+       public Task<HomeViewModel> GetHomeViewModelAsync(int? categoryId, int? brandId, int pageId);
     }
 }
